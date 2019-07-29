db4edd6098193cbc0619ed8bd8fbbb2a8bc16768
Maroder10885 <aledor10885@gmail.com>
2019-07-29 21:56:51 +0300

    вычисления

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-let money = 'Ваш бюджет на месяц?';
-let time = 'Введите дату в формате YYYY-MM-DD';
+let money = prompt('Ваш бюджет на месяц?', '');
+let time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
 let appData = {
-    money,
+    budget:money,
     timeData: time,
-    
-    expenses:{
-        'ответ на первый вопрос' : 'ответ на второй вопрос'
-    },
-
-    optionalExpenses:{
-
-    },
-
+    expenses:{},
+    optionalExpenses:{},
     income: [],
-
     savings: false
 
 };
+
+let a1 = prompt('Введите обязательную статью расходов в этом месяце', ''),
+    a2 = prompt('Во сколько обойдется?', ''),
+    a3 = prompt('Введите обязательную статью расходов в этом месяце', ''),
+    a4 = prompt('Во сколько обойдется?', '');
+
+    appData.expenses.a1 = a2;
+    appData.expenses.a3 = a4;
+
+    alert(appData.budget/30);
+
